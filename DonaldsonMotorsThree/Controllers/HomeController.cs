831faf9ed02d08ef6a6514cabf94a99e9615b102
Jordan Porter <jordan.1.porter@hotmail.co.uk>
2018-04-17 18:49:13 +0100

    Edited Contact to include Donaldson Contact Links.

--- DonaldsonMotorsThree/Controllers/HomeController.cs
+++ DonaldsonMotorsThree/Controllers/HomeController.cs
@@ -29,7 +29,7 @@ namespace DonaldsonMotorsThree.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            ViewBag.Message = "Contact Us.";
 
             return View();
         }
