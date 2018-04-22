14cb2de7d7622dac9db6c7daa577ac4d35e506c8
Jordan Porter <jordan.1.porter@hotmail.co.uk>
2018-04-22 02:55:53 +0100

    Added Jobs and Suppliers Api Controllers and Job+Supplier DTO Classes

---
+++ DonaldsonMotorsThree/Dtos/JobDto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DonaldsonMotorsThree.Dtos
+{
+    public class JobDto
+    {
+
+        [Key]
+        public int JobId { get; set; }
+
+        public int CustomerId { get; set; }
+
+        public int VehicleId { get; set; }
+
+        public int PartId { get; set; }
+
+        public string CompletedBy { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DateCompleted { get; set; }
+
+        [Required]
+        public string JobRequirements { get; set; }
+
+        [Required]
+        public decimal JobCost { get; set; }
+
+        [Required]
+        public int JobStatus { get; set; }
+
+    }
+}
---
+++ DonaldsonMotorsThree/Dtos/SupplierDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DonaldsonMotorsThree.Dtos
+{
+    public class SupplierDto
+    {
+
+        [Key]
+        public int SupplierId { get; set; }
+
+        [Required]
+        public string SupplierName { get; set; }
+
+        [Required]
+        public string AddressLine1 { get; set; }
+
+        public string AddressLine2 { get; set; }
+
+        public string Town { get; set; }
+
+        [Required]
+        public string Postcode { get; set; }
+
+        [Required]
+        public string TelephoneNumber { get; set; }
+
+        [Required]
+        public string ContactName { get; set; }
+
+        [Required]
+        public string EmailAddress { get; set; }
+
+    }
+}
