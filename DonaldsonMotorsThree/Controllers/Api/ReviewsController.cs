e1ec4ecb3b5c056f8e69d8d8e8a9942c7cb90bb2
Jordan Porter <jordan.1.porter@hotmail.co.uk>
2018-05-01 19:20:55 +0100

    Added ReviewApi and StaffDataApi Created Review and Staff Repos and DTOs

--- DonaldsonMotorsThree/Controllers/Api/ReviewsController.cs
+++ DonaldsonMotorsThree/Controllers/Api/ReviewsController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using AutoMapper;
+using DonaldsonMotorsThree.Dtos;
+using DonaldsonMotorsThree.Models;
 
 namespace DonaldsonMotorsThree.Controllers.Api
 {
@@ -11,6 +15,96 @@ namespace DonaldsonMotorsThree.Controllers.Api
     {
         // This is the Reviews API controller // 
         // The controller is used to direct data services within the application for reviews and feedback functions //
+
+        //Declare Context // 
+        private ApplicationDbContext _context;
+
+        // Initialise DbContext in constructor //
+        public ReviewsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+
+        //GET /api/reviews
+        public IEnumerable<ReviewDto> GetReviews()
+        {
+            return _context.Reviews.ToList().Select(Mapper.Map<Review, ReviewDto>);
+
+        }
+
+        //GET /api/reviews/1
+        public IHttpActionResult GetReview(int id)
+        {
+            var review = _context.Reviews.SingleOrDefault(r => r.ReviewId == id);
+            // if review returned is null, return not found error // 
+            if (review == null)
+                NotFound();
+            // else return OK with maper mapping review DTO to review //
+
+            return Ok(Mapper.Map<Review, ReviewDto>(review));
+        }
+
+        //POST /api/reviews
+        [HttpPost]
+        public IHttpActionResult CreateReview(ReviewDto reviewDto)
+        {
+            // if model state is not valid, return bad request // 
+            if (!ModelState.IsValid)
+                return BadRequest();
+            //else
+            var review = Mapper.Map<ReviewDto, Review>(reviewDto);
+
+            //add review to database and save changes // 
+
+            _context.Reviews.Add(review);
+            _context.SaveChanges();
+
+            // Check that review Dto Id and review id match // 
+            reviewDto.ReviewId = review.ReviewId;
+
+            // return created URI // 
+
+            return Created(new Uri(Request.RequestUri + "/" + review.ReviewId), reviewDto);
+        }
+
+        [HttpPut]
+        public void UpdateReview(int id, ReviewDto reviewDto)
+        {
+            // if model state is valid, throw new bad request // 
+            if(!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+
+            // review in db is assigned to review through id match //
+            var reviewInDb = _context.Reviews.SingleOrDefault(r => r.ReviewId == id);
+
+            // if review is null, throw http not found // 
+            if(reviewInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            // Map review DTO to reviewInDb // 
+            Mapper.Map(reviewDto, reviewInDb);
+
+            // save changes //
+            _context.SaveChanges();
+        }
+
+        //DELETE /api/reviews/1
+        [HttpDelete]
+        public void DeleteReview(int id)
+        {
+            // Create reviewInDb through id match // 
+            var reviewInDb = _context.Reviews.SingleOrDefault(r => r.ReviewId == id);
+
+            //if reviewInDb is null, throw not found http status code // 
+            if(reviewInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            //else remove reviewInDb and save changes // 
+            _context.Reviews.Remove(reviewInDb);
+            _context.SaveChanges();
+
+        }
 
 
     }
--- DonaldsonMotorsThree/Controllers/Api/StaffDataController.cs
+++ DonaldsonMotorsThree/Controllers/Api/StaffDataController.cs
@@ -25,6 +25,7 @@ namespace DonaldsonMotorsThree.Controllers.Api
             return _context.Staff.ToList().Select(Mapper.Map<Staff, StaffDto>);
         }
 
+        //GET /api/staffdata/1
         public IHttpActionResult GetStaff(int id)
         {
             // Search the context for staff using id 
@@ -34,6 +35,68 @@ namespace DonaldsonMotorsThree.Controllers.Api
             if (staff == null)
                 return NotFound();
             return Ok(Mapper.Map<Staff,StaffDto>(staff));
+        }
+
+
+        //POST /api/staffdata
+        [HttpPost]
+        public IHttpActionResult CreateStaff(StaffDto staffDto)
+        {
+            // If model state is not valid then return bad request//
+            if (!ModelState.IsValid)
+                return BadRequest();
+            //else
+            var staff = Mapper.Map<StaffDto, Staff>(staffDto);
+
+            // add job to database and save changes //
+            _context.Staff.Add(staff);
+            _context.SaveChanges();
+
+
+            // check that JobDto Id and Job Id match//
+           staffDto.EmployeeId = staff.EmployeeId;
+
+            // Return created URI //
+            return Created(new Uri(Request.RequestUri + "/" + staff.EmployeeId), staffDto);
+
+        }
+
+        [HttpPut]
+        public void UpdateStaff(int id, StaffDto staffDto)
+        {
+            // If model state is valid, throw new bad request // 
+            if(!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+        
+
+            // staffInDb is assigned to staff object through id match // 
+            var staffInDb = _context.Staff.SingleOrDefault(s => s.EmployeeId == id);
+
+            // if staff is null throw new http not found status code //
+            if (staffInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            // use mapper to map staffDto to staffInDb // 4
+            Mapper.Map(staffDto, staffInDb);
+
+            //Save changes //
+            _context.SaveChanges();
+        }
+
+        [HttpDelete]
+        public void DeleteStaff(int id)
+        {
+            // Create staffInDb which is assigned to staff through id match //
+            var staffInDb = _context.Staff.SingleOrDefault(s => s.EmployeeId == id);
+
+            // if staffInDb is null throw not found //
+            if(staffInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            //else remove staffInDb from db and save changes // 
+            _context.Staff.Remove(staffInDb);
+            _context.SaveChanges();
+
         }
 
 
